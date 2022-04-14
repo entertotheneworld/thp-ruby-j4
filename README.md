11aefe47aefd2aebbc3e1683140233492c9792c4
entertotheneworld <tristan.greffe@hotmail.com>
2022-04-14 12:43:29 +0200

    First version

---
+++ README.md
@@ -0,0 +1,20 @@
+# THP JOUR 4
+
+## 1. Download the source code
+
+```bash
+$ git clone https://github.com/entertotheneworld/thp-ruby-j4.git
+$ cd ruby_j4
+$ cd lib
+```
+
+## 2. Testing
+
+run the exercise of your choice
+
+```bash
+$ ruby 00_journalists.rb
+$ ruby 01_cryptocurrencies.rb
+```
+
+## 3. Enjoy
---
+++ lib/00_journalists.rb
@@ -0,0 +1,119 @@
+@journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def main 
+    puts "----------------------------------------------------"
+    tab = ["Combien y a t-il de handle ?","Quel est le handle le plus court ?","Combien y-a-t'il de hanle contenant 5 caractères ?","Combien de handle commencent par une majucule ?", "Liste des handle par ordre alphabétique", "Liste des handle trier par taille", "Position de la personne @epenser","Répartition des handle par taille (nb handle avec 1 caractre, nb handle avec 2 caractere etc.)","Sortir du programme"]
+    tab.each_with_index{|val,i| puts "#{i+1}. #{val}"}
+    puts "----------------------------------------------------"
+    choice = 0
+    while choice < 1 || choice > 9
+        puts "Votre choix :"
+        choice = gets.chomp.to_i
+    end
+    return choice
+end
+
+def number_handle
+    puts "Il y as #{@journalists.length} handle dans cette array"
+end
+
+def shortest_handle
+    shortest_word = ""
+    @journalists.each_with_index {|val, i|
+        # Initialisation : shortest_word devient le premier mot de la liste --cette ligen ne sera exécuter qu'au premier tour
+        shortest_word = val if i == 0
+        # Comparaison 
+        shortest_word = val if val.length < shortest_word.length
+    }
+    puts "Comme par hasard, le handle le plus petit est : #{shortest_word}"
+end
+
+def handle_five_characters
+    five_word = []
+    @journalists.each {|val|
+        five_word << val if val.length == 6
+    }
+    puts "Il y as #{five_word.length} handle qui ont 5 caractéres. Ce sont les suivants :"
+    puts five_word
+end
+
+def handle_start_uppercase
+    number_words = 0
+    @journalists.each {|val|
+        # Céer un tableau contenant chaque lettre. exemple 1er mot @jcunniet = ["@","j","c","u","n","n","i","e","t"]
+        word_board = val.split(//)
+        # On incremente si la seconde lettre du mot est en majucule
+        number_words += 1 if word_board[1] === word_board[1].upcase
+    }
+    puts "Il y as #{number_words} handle qui commencent par une majucule"
+end
+
+def alphabetical_order
+    # On pourrais utiliser .tr afin de supprimer les caractére (@ et _) et ainsi avoir une vrai liste trier alphétiquement mais je n'arrive pas à joindre les deux tableaux
+    alphabetical_list = []
+    @journalists.each_with_index {|value, index|
+        alphabetical_list << value.downcase
+    }
+    puts "Voici la liste par ordre alphabetique :\n#{alphabetical_list.sort}"
+end
+
+def size_handle
+    size_list = @journalists.sort {|x,y| x.length <=> y.length}
+    
+    return size_list
+end
+
+def handle_position
+    position = @journalists.index("@epenser")
+    puts "@epenser est à la position #{position}"
+end
+
+def repartition_handle
+    size_list = size_handle
+    repartition = {}
+    size_list.each_with_index {|val,i|
+        if repartition[val.length]
+            repartition[val.length] += 1
+        else 
+            repartition[val.length] = 1
+        end
+    }
+    repartition.each do |carac,count|
+        puts "Il y as #{count} handle avec #{carac} caractére"
+    end
+end
+
+def perform
+    choice = main
+    case choice
+    when 1
+        number_handle
+    when 2
+        shortest_handle
+    when 3
+        handle_five_characters
+    when 4
+        handle_start_uppercase
+    when 5
+        puts "La tu m'en demande trop"
+        puts "Appuyer sur entree pour quitter"
+        gets.chomp.to_i
+        puts "Non, je rigole, ce n'est pas parfait et j'ai galérer mais j'y suis arrivé !!"
+        alphabetical_order
+    when 6
+        size_list = size_handle
+        puts "Le tableau trier par taille croissante :\n#{size_list}"
+    when 7
+        handle_position
+    when 8
+        repartition_handle
+    when 9
+        puts "À bientôt ! On t'attend pour encore plus d'aventures"
+        return choice
+    end
+end
+
+loop do 
+    choice = perform
+    break if choice == 9
+end
